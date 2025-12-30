134adc24ef066611c0bc163ce7530b9de4271bb0
Ahmed Landoulsi <landoulsi.ahmed@gmail.com>
2025-12-30 20:59:06 +0400

    Inject tracer interceptor into OkHttp builder.

--- gradle-plugin/src/main/kotlin/io/tracer/plugin/OkHttpBuilderVisitor.kt
+++ gradle-plugin/src/main/kotlin/io/tracer/plugin/OkHttpBuilderVisitor.kt
@@ -38,7 +38,66 @@ class OkHttpBuilderVisitor(nextVisitor: ClassVisitor) : ClassVisitor(Opcodes.ASM
             ) {
                 println("Tracer: Injecting interceptor into method call in $owner.$name")
                 // Stack before: [Builder]
-                
+
+                val loopStart = org.objectweb.asm.Label()
+                val loopEnd = org.objectweb.asm.Label()
+                val hasLabel = org.objectweb.asm.Label()
+
+                // Duplicate builder for later use
+                visitInsn(Opcodes.DUP) // [Builder, Builder]
+                // Call interceptors() on the dup
+                visitMethodInsn(
+                    Opcodes.INVOKEVIRTUAL,
+                    "okhttp3/OkHttpClient\$Builder",
+                    "interceptors",
+                    "()Ljava/util/List;",
+                    false
+                ) // [Builder, List]
+                // Get iterator
+                visitMethodInsn(
+                    Opcodes.INVOKEINTERFACE,
+                    "java/util/List",
+                    "iterator",
+                    "()Ljava/util/Iterator;",
+                    true
+                ) // [Builder, Iterator]
+
+                visitLabel(loopStart)
+                visitInsn(Opcodes.DUP) // [Builder, Iterator, Iterator]
+                visitMethodInsn(
+                    Opcodes.INVOKEINTERFACE,
+                    "java/util/Iterator",
+                    "hasNext",
+                    "()Z",
+                    true
+                )
+                visitJumpInsn(Opcodes.IFEQ, loopEnd)
+                visitInsn(Opcodes.DUP) // [Builder, Iterator, Iterator]
+                visitMethodInsn(
+                    Opcodes.INVOKEINTERFACE,
+                    "java/util/Iterator",
+                    "next",
+                    "()Ljava/lang/Object;",
+                    true
+                )
+                visitTypeInsn(Opcodes.INSTANCEOF, "io/tracer/TracerInterceptor")
+                visitJumpInsn(Opcodes.IFNE, hasLabel)
+                visitJumpInsn(Opcodes.GOTO, loopStart)
+
+                // Found existing tracer interceptor
+                visitLabel(hasLabel)
+                visitInsn(Opcodes.POP) // iterator
+                visitInsn(Opcodes.POP) // iterator dup
+                visitInsn(Opcodes.POP) // original builder dup
+                // Call original build()
+                super.visitMethodInsn(opcode, owner, name, descriptor, isInterface)
+                return
+
+                // No tracer interceptor found; clean up iterator
+                visitLabel(loopEnd)
+                visitInsn(Opcodes.POP) // iterator
+                // Stack: [Builder] (original dup consumed, original builder remains)
+
                 // Inject: .addInterceptor(new TracerInterceptor())
                 visitTypeInsn(Opcodes.NEW, "io/tracer/TracerInterceptor")
                 visitInsn(Opcodes.DUP)
@@ -49,8 +108,6 @@ class OkHttpBuilderVisitor(nextVisitor: ClassVisitor) : ClassVisitor(Opcodes.ASM
                     "()V",
                     false
                 )
-                // Stack: [Builder, Interceptor]
-                
                 visitMethodInsn(
                     Opcodes.INVOKEVIRTUAL,
                     "okhttp3/OkHttpClient\$Builder",
@@ -59,7 +116,7 @@ class OkHttpBuilderVisitor(nextVisitor: ClassVisitor) : ClassVisitor(Opcodes.ASM
                     false
                 )
                 // Stack: [Builder] (returned from addInterceptor)
-                
+
                 // Now call original build()
                 super.visitMethodInsn(opcode, owner, name, descriptor, isInterface)
             } else {
